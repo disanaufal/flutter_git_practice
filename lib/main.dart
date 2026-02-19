dc0c053220793a8067f64e0ad13d6e027da7d0bd
disanaufal <disanaufalrifqi@gmail.com>
2026-02-19 09:02:17 +0700

    Add Project Structure section in README

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  // TODO: Implement login screen UI
 }
 
 class MyApp extends StatelessWidget {
@@ -55,6 +56,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int maxRetryCount = 10;
 
   void _incrementCounter() {
     setState(() {
