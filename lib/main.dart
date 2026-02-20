b50287dc6ef058be0f47cd74bf2d5a44ac617704
disanaufal <disanaufalrifqi@gmail.com>
2026-02-20 13:56:18 +0700

    Raplace not functional button

--- lib/main.dart
+++ lib/main.dart
@@ -54,19 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int maxRetryCount = 10;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -93,11 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
           },
           child: const Text('Press Me'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
